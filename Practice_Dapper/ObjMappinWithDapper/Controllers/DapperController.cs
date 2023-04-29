dcbb9b5372e251b4971af6b2e956f641fe419381
sajjad0057 <sajjadhossain0057@gmail.com>
2023-04-29 18:19:19 +0600

    Successfully update and delete data using dapper and dynamic parameters

--- Practice_Dapper/ObjMappinWithDapper/Controllers/DapperController.cs
+++ Practice_Dapper/ObjMappinWithDapper/Controllers/DapperController.cs
@@ -60,17 +60,93 @@ public class DapperController : ControllerBase
 
             using (var connection = new SqlConnection(_CONNECTION_STRING))
             {
-                var result = await connection.ExecuteAsync(sql.ToString(), dynamicParameters);
+                await connection.OpenAsync();
 
-                if (result > 0)
-                    return Ok($"{result} row is affected");
-                else
-                    return BadRequest("There have a problem occured in inserting new row");
+                using(var _transaction = connection.BeginTransaction())
+                {
+                    var result = await connection.ExecuteAsync(sql.ToString(), dynamicParameters, transaction: _transaction);
+                    if (result > 0)
+                    {
+                        _transaction.Commit();
+                        return Ok($"{result} row is affected");
+                    }
+                    else
+                        return BadRequest("There have a problem occured in inserting new row");
+                }
             }
         }
         else
         {
             return BadRequest("Person can't be null!");
+        }
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody]Person person)
+    {
+        var sql = new StringBuilder(@"UPDATE [dbo].[Person] SET 
+                                        Title = @Title, 
+                                        FirstName = @FirstName, 
+                                        LastName = @LastName, 
+                                        Gender = @Gender 
+                                        WHERE Id = @Id");
+        if (person is not null)
+        {
+            var dynamicParameters = new DynamicParameters();
+
+            foreach (var item in person.GetType().GetProperties())
+            {
+                //Console.WriteLine($"{item.Name} , {item.GetValue(person)}");
+                dynamicParameters.Add($"{item.Name}", $"{item.GetValue(person)}");
+            }
+
+            using (var connection = new SqlConnection(_CONNECTION_STRING))
+            {
+                await connection.OpenAsync();
+
+                using (var _transaction = connection.BeginTransaction())
+                {
+                    var result = await connection.ExecuteAsync(sql.ToString(), dynamicParameters, transaction: _transaction);
+                    if (result > 0)
+                    {
+                        _transaction.Commit();
+                        return Ok($"{result} row is affected");
+                    }
+                    else
+                        return BadRequest("There have a problem occured in updating row");
+                }
+            }
+        }
+        else
+        {
+            return BadRequest("Person can't be null!");
+        }
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromQuery]int Id)
+    {
+        var sql = new StringBuilder(@"DELETE FROM [dbo].[Person] 
+                                        WHERE Id = @Id");
+
+        var dynamicParameters = new DynamicParameters();
+        dynamicParameters.Add("Id", $"{Id}");
+
+        using (var connection = new SqlConnection(_CONNECTION_STRING))
+        {
+            await connection.OpenAsync();
+
+            using (var _transaction = connection.BeginTransaction())
+            {
+                var result = await connection.ExecuteAsync(sql.ToString(), dynamicParameters, transaction: _transaction);
+                if (result > 0)
+                {
+                    _transaction.Commit();
+                    return Ok($"{result} row is deleted");
+                }
+                else
+                    return BadRequest("There have a problem occured in deleteing row");
+            }
         }
     }
 }
