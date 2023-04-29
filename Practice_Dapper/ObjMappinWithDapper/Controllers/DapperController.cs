6fb4009fb725009284b86e47f8d2a239b2adb29c
sajjad0057 <sajjadhossain0057@gmail.com>
2023-04-29 13:17:07 +0600

    Get data successfully with initial logic

---
+++ Practice_Dapper/ObjMappinWithDapper/Controllers/DapperController.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using ObjMappinWithDapper.Models;
+using System.Data.SqlClient;
+
+namespace ObjMappinWithDapper.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DapperController : ControllerBase
+{
+    private readonly string? _CONNECTION_STRING;
+    public DapperController(IConfiguration configuration) => 
+        _CONNECTION_STRING = configuration?.GetConnectionString("DefaultConnection");
+
+    [HttpGet("")]
+    public async Task<IActionResult> Index()
+    {
+        var sql = @"SELECT 
+	           [Title]
+              ,[FirstName]
+              ,[LastName]
+              ,[Gender]
+              FROM [Practice_Dapper1].[dbo].[Person]";
+
+        using (var connection = new SqlConnection(_CONNECTION_STRING))
+        {
+            var persons = await connection.QueryAsync<Person>(sql);
+
+            return Ok(persons);
+        }
+    }
+}
---
+++ Practice_Dapper/ObjMappinWithDapper/Models/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace ObjMappinWithDapper.Models;
+
+public class Person
+{
+    public string Title { get; set; } = string.Empty;
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Gender { get; set; } = string.Empty;
+}
