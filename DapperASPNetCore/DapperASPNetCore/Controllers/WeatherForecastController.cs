f980fec42c198190bf22fbe647e609e2c181b357
sajjad0057 <sajjadhossain0057@gmail.com>
2023-04-29 22:49:04 +0600

    configure log4net for track log

--- DapperASPNetCore/DapperASPNetCore/Controllers/WeatherForecastController.cs
+++ DapperASPNetCore/DapperASPNetCore/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace DapperASPNetCore.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -21,6 +21,10 @@ namespace DapperASPNetCore.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogDebug("Hey, this is a DEBUG message.");
+            _logger.LogInformation("Hey, this is an INFO message.");
+            _logger.LogWarning("Hey, this is a WARNING message.");
+            _logger.LogError("Hey, this is an ERROR message.");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
--- DapperASPNetCore/DapperASPNetCore/Program.cs
+++ DapperASPNetCore/DapperASPNetCore/Program.cs
@@ -1,13 +1,22 @@
+using log4net;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Logging.AddLog4Net();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+var log = LogManager.GetLogger(typeof(Program));
+
 var app = builder.Build();
+
+log.Info("Application starting up");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
